60afaed8f737dbafc1239d88ac2b0dcac7a202ea
Lavanyayerrakula <yerrakulalavanya.8888@gmail.com>
2026-03-30 01:19:45 +0000

    load

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 /* add your code here */
+document.addEventListener('DOMContentLoaded', () => {
+   const stocksData = JSON.parse(stockContent);
+   const userData = JSON.parse(userContent);
 
+   console.log({userData});
+});
